c68654c1aecd25dfbe398ee63aa2f7bd243cbc38
Abeer <abeer.technology@gmail.com>
2022-12-04 11:11:10 +0600

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue"
+import axios from "axios"
+
+const email = ref("")
+const pw = ref("")
+
+const submit = async () => {
+  try {
+    const { data } = await axios.post(
+      "http://localhost:4050/new",
+      { name: "Abeer" },
+      { withCredentials: true }
+    )
+
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <input type="text" placeholder="email" v-model="email" />
+  <input type="password" placeholder="password" v-model="pw" />
+  <button @click="submit">Submit</button>
 </template>
+
+<style></style>
