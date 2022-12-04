2d9851c58ee4b96740e3e384aa7ce4536001ad19
Abeer <abeer.technology@gmail.com>
2022-12-04 10:56:45 +0600

    commit

---
+++ src/views/NewPage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue"
+import axios from "axios"
+
+const email = ref("")
+const pw = ref("")
+
+const submit = async () => {
+  try {
+    const { data } = await axios.post(
+      "http://localhost:4050/new",
+      { name: "Abeer" },
+      { withCredentials: true }
+    )
+
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<template>
+  <input type="text" placeholder="email" v-model="email" />
+  <input type="password" placeholder="password" v-model="pw" />
+  <button @click="submit">Submit</button>
+</template>
+
+<style></style>
